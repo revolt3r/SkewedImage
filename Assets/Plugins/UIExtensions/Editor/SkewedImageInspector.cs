6b5de5019bf1c3966b48bc0fbe10aee6d2544010
Vladimir Grati <vladimir.grati@fluffyfairygames.com>
2018-08-16 23:54:03 +0200

    SkewImage v1.0

---
+++ Assets/Plugins/UIExtensions/Editor/SkewedImageInspector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+
+namespace UIExtensions.Editor
+{
+    [CustomEditor(typeof(SkewedImage))]
+    public class SkewedImageInspector : ImageEditor
+    {
+        private SkewedImage _skewedImage;
+        private SerializedProperty _skewVector;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            _skewedImage = (SkewedImage)target;
+            _skewVector = serializedObject.FindProperty("SkewVector");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(_skewVector);
+
+            if (_skewVector.vector2Value != _skewedImage.SkewVector)
+            {
+                Undo.RecordObject(_skewedImage, "Changed Skew");
+                _skewedImage.SkewVector = _skewVector.vector2Value;
+                _skewedImage.OnRebuildRequested();
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Assets/Plugins/UIExtensions/SkewedImage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UIExtensions
+{
+    public class SkewedImage : Image
+    {
+        public Vector2 SkewVector;
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            base.OnPopulateMesh(vh);
+            var r = GetPixelAdjustedRect();
+            var v = new Vector4(r.x, r.y, r.x + r.width, r.y + r.height);
+            Color32 color32 = color;
+            vh.Clear();
+            vh.AddVert(new Vector3(v.x - SkewVector.x, v.y - SkewVector.y), color32, new Vector2(0f, 0f));
+            vh.AddVert(new Vector3(v.x + SkewVector.x, v.w - SkewVector.y), color32, new Vector2(0f, 1f));
+            vh.AddVert(new Vector3(v.z + SkewVector.x, v.w + SkewVector.y), color32, new Vector2(1f, 1f));
+            vh.AddVert(new Vector3(v.z - SkewVector.x, v.y + SkewVector.y), color32, new Vector2(1f, 0f));
+            vh.AddTriangle(0, 1, 2);
+            vh.AddTriangle(2, 3, 0);
+        }
+    }
+}
